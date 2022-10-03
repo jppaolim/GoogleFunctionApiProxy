36e2ce4d7061602646b66b3b01b868d3dd28757f
Jean-Paul Paoli <contact@paolim.fr>
2022-10-03 13:42:06 +0200

    fonctionne local et pas sur google cloud

--- index.js
+++ index.js
@@ -8,37 +8,83 @@ const CONFIG = {
 	upstream: process.env.UPSTREAMSERVER,
 };
 
-//const functions = require('@google-cloud/functions-framework');
 
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const bodyParser = require('body-parser');
+const queryString = require('querystring');
+//const functions = require('@google-cloud/functions-framework');
+//var cors = require("cors");
 
 const options = {
   target: CONFIG.upstream, // target host
   changeOrigin: true, // needed for virtual hosted sites
   ws: true, // proxy websockets
+  logLevel: "debug",
   logger: console,
+  followRedirects: true,
+  secure: false,
 
-  onProxyRes(proxyRes) {
-    proxyRes.headers['x-added'] = 'foobar'; // add new header to response
-    delete proxyRes.headers['x-removed']; // remove header from response
+  onProxyReq: (proxyReq, req, res) => {
+    /**
+     * @type {null | undefined | object}
+     */
+    // @ts-ignore
+    const body = req.body;
+    // Restream parsed body before proxying
+    // https://github.com/http-party/node-http-proxy/blob/master/examples/middleware/bodyDecoder-middleware.js
+    if (!body || !Object.keys(body).length) {
+      return;
+    }
+    const contentType = proxyReq.getHeader('Content-Type');
+    let contentTypeStr = Array.isArray(contentType) ? contentType[0] : contentType.toString();
+    // Grab 'application/x-www-form-urlencoded' out of 'application/x-www-form-urlencoded; charset=utf-8'
+    contentTypeStr = contentTypeStr.match(/^([^;]*)/)[1];
+
+    let bodyData;
+    if (contentTypeStr === 'application/json') {
+      bodyData = JSON.stringify(body);
+    }
+
+    if (contentTypeStr === 'application/x-www-form-urlencoded') {
+      bodyData = queryString.stringify(body);
+    }
+
+    if (bodyData) {
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
   }
-};
+  //onProxyRes(proxyRes) {
+  //  proxyRes.headers['x-added'] = 'foobar'; // add new header to response
+  //  delete proxyRes.headers['x-removed']; // remove header from response
+  //}
 
+  
+};
 
 // create the proxy (without context)
 const aProxy = createProxyMiddleware(options);
+//const aProxy  = proxy(options);
+
 
 // mount `aProxy` in web server
 const app = express();
-app.use('/*', aProxy);
-app.listen(3000);
+
+// Allow CORS from any origin
+//app.use(cors());
+//app.set('trust proxy', true);
+
+app.use('/', aProxy);
+//app.listen(process.env.PORT);
+// Optional: Support special POST bodies - requires restreaming (see below)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 module.exports = {
 	proxy: app
+//nope veille version  proxy: aProxy
 }
 
-//functions.http('proxy', (req, res) => {
-//    return aproxy(req, res);
-//});

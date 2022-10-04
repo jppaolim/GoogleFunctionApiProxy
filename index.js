00f5af47d1bfc4198e199b125687fa08ab139a70
Jean-Paul Paoli <contact@paolim.fr>
2022-10-04 22:56:11 +0200

    final, works locally, not remotely

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // @ts-check
 
+// Simple proxy ... works locally but not on google function due to Huggingface API 
+// what I tried :
+//ws: true, // proxy websocket 
+//followRedirects: true,
+//secure: true,
+//preserveHeaderKeyCase: true,
+//autoRewrite:true,
+//protocolRewrite: true,
+// Allow CORS from any origin
+//app.use(cors());
+//app.set('trust proxy', true);
+//toProxy: true,
+
+//set to true, it won't work locally and I suspect this breaks down on google  
+//xfwd: true, 
+
+
 //load the config env with API Key 
 require('yamlenv').config();
 
@@ -8,83 +25,38 @@ const CONFIG = {
 	upstream: process.env.UPSTREAMSERVER,
 };
 
-
+//now start creating the proxy 
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const morgan = require("morgan");
+const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
 
-const bodyParser = require('body-parser');
-const queryString = require('querystring');
-//const functions = require('@google-cloud/functions-framework');
-//var cors = require("cors");
-
+//options for the proxy 
 const options = {
-  target: CONFIG.upstream, // target host
+  target: CONFIG.upstream,  
   changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
-  logLevel: "debug",
-  logger: console,
-  followRedirects: true,
-  secure: false,
-
-  onProxyReq: (proxyReq, req, res) => {
-    /**
-     * @type {null | undefined | object}
-     */
-    // @ts-ignore
-    const body = req.body;
-    // Restream parsed body before proxying
-    // https://github.com/http-party/node-http-proxy/blob/master/examples/middleware/bodyDecoder-middleware.js
-    if (!body || !Object.keys(body).length) {
-      return;
-    }
-    const contentType = proxyReq.getHeader('Content-Type');
-    let contentTypeStr = Array.isArray(contentType) ? contentType[0] : contentType.toString();
-    // Grab 'application/x-www-form-urlencoded' out of 'application/x-www-form-urlencoded; charset=utf-8'
-    contentTypeStr = contentTypeStr.match(/^([^;]*)/)[1];
-
-    let bodyData;
-    if (contentTypeStr === 'application/json') {
-      bodyData = JSON.stringify(body);
-    }
-
-    if (contentTypeStr === 'application/x-www-form-urlencoded') {
-      bodyData = queryString.stringify(body);
-    }
-
-    if (bodyData) {
-      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-      proxyReq.write(bodyData);
-    }
-  }
-  //onProxyRes(proxyRes) {
-  //  proxyRes.headers['x-added'] = 'foobar'; // add new header to response
-  //  delete proxyRes.headers['x-removed']; // remove header from response
-  //}
-
   
+  // very import for POST request 
+  onProxyReq: fixRequestBody,
+  logger: console,
+  logLevel: "debug"
 };
 
 // create the proxy (without context)
 const aProxy = createProxyMiddleware(options);
-//const aProxy  = proxy(options);
 
-
-// mount `aProxy` in web server
+// create the server
 const app = express();
 
-// Allow CORS from any origin
-//app.use(cors());
-//app.set('trust proxy', true);
+//do the login 
+morgan.token('req-headers', function(req,res){
+  return JSON.stringify(req.headers)
+ })
+app.use(morgan(':method :url :status :req-headers '));
 
+// mount `aProxy` in web server
 app.use('/', aProxy);
-//app.listen(process.env.PORT);
-// Optional: Support special POST bodies - requires restreaming (see below)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 
 module.exports = {
 	proxy: app
-//nope veille version  proxy: aProxy
 }
 
